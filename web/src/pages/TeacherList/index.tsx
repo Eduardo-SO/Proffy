fb931b1d45cd48770fbebd38dfba615ea5b25086
Eduardo SO <eduardo.sool07@gmail.com>
2020-08-04 23:54:23 -0300

    add: Content and mobile styles

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -2,7 +2,17 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 
-import { Container, SearchTeachers, InputBlock } from './styles';
+import whatsappIcon from '../../images/icons/whatsapp.svg';
+
+import { 
+  Container, 
+  SearchTeachers, 
+  InputBlock,
+  Content,
+  TeacherItem,
+  ItemHeader,
+  ItemFooter
+} from './styles';
 
 const TeacherList: React.FC = () => {
   return (
@@ -25,6 +35,35 @@ const TeacherList: React.FC = () => {
           </InputBlock>
         </SearchTeachers>
       </PageHeader>
+      <Content>
+        <TeacherItem>
+          <ItemHeader>
+            <img src="https://avatars2.githubusercontent.com/u/54240849?s=460&u=1726068c37491bf0147a99458ac086305b706f81&v=4" alt="Eduardo SO"/>
+
+            <div>
+              <strong>Eduardo SO</strong>
+              <span>Física</span>
+            </div>
+          </ItemHeader>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, cum!
+            <br /> <br />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos amet placeat excepturi deserunt doloribus, saepe suscipit nam nobis quas recusandae labore ad maiores facere perferendis itaque ratione neque ex iure.
+          </p>
+
+          <ItemFooter>
+            <p>
+              Preço/hora
+              <strong>R$ 80,00</strong>
+            </p>
+            <button type="button">
+              <img src={whatsappIcon} alt="Whatsapp"/>
+              Entrar em contato
+            </button>
+          </ItemFooter>
+        </TeacherItem>
+      </Content>
     </Container>
   );
 }
--- web/src/pages/TeacherList/styles.ts
+++ web/src/pages/TeacherList/styles.ts
@@ -72,3 +72,98 @@ export const InputBlock = styled.div`
     }
   }
 `;
+
+export const Content = styled.main`
+  width: 90%;
+  margin: 3.2rem auto;
+`;
+
+export const TeacherItem = styled.article`
+  overflow: hidden;
+
+  margin-top: 2.4rem;
+  border: 1px solid var(--color-line-in-white);
+  border-radius: .8rem;
+
+  background: var(--color-box-base);
+
+  > p {
+    padding: 0 2rem;
+    font-size: 1.6rem;
+    line-height: 2.8rem;
+  }
+`;
+
+export const ItemHeader = styled.header`
+  display: flex;
+  align-items: center;
+
+  padding: 3.2rem 2rem;
+
+  img {
+    width: 8rem;
+    height: 8rem;
+
+    border-radius: 50%;
+  }
+
+  div {
+    margin-left: 2.4rem;
+
+    strong {
+      display: block;
+      font: 700 2.4rem Archivo;
+      color: var(--color-text-title);
+    }
+
+    span {
+      display: block;
+      margin-top: .4rem;
+      font-size: 1.6rem;
+    }
+  }
+`;
+
+export const ItemFooter = styled.footer`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  margin-top: 3.2rem;
+  padding: 3.2rem 2rem;
+  border-top: 1px solid var(--color-line-in-white);
+
+  background: var(--color-box-footer);
+
+  p {
+    strong {
+      display: block;
+
+      color: var(--color-primary);
+      font-size: 1.6rem;
+    }
+  }
+
+  button {
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+
+    width: 20rem;
+    height: 5.6rem;
+    border: 0;
+    border-radius: 0.8rem;
+
+    background: var(--color-secundary);
+    color: var(--color-button-text);
+
+    font: 700 1.4rem Archivo;
+    cursor: pointer;
+
+    transition: .2s;
+
+    &:hover {
+      background: var(--color-secundary-dark);
+    }
+  }
+`;
