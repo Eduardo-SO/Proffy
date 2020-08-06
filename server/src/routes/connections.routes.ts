5b6e86d8c0ec2b0609b6db33a01be7dc4946c876
Eduardo SO <eduardo.sool07@gmail.com>
2020-08-05 21:59:33 -0300

    Create connections method

---
+++ server/src/routes/connections.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import ConnectionsController from '../controllers/ConnectionsController';
+
+const connectionsController = new ConnectionsController();
+
+const classesRouter = Router();
+
+// classesRouter.get('/', connectionsController.index);
+classesRouter.post('/', connectionsController.create);
+
+export default classesRouter;
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import classesRouter from './classes.routes';
+import connectionsRouter from './connections.routes';
 
 const routes = Router();
 
 routes.use('/classes', classesRouter);
+routes.use('/connections', connectionsRouter);
 
 export default routes;
