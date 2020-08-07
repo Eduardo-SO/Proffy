3fbf3e38b402ee36a2ffe787920acc1ea4995470
Eduardo SO <eduardo.sool07@gmail.com>
2020-08-07 20:03:17 -0300

    Import landing image

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Image } from 'react-native';
 
 import { Container, Title } from './styles';
+
+import landingImage from '../../assets/images/landing.png';
 
 const Landing: React.FC = () => {
   return (
     <Container>
+      <Image source={landingImage} />
       <Title>Hello Landing!</Title>
     </Container>
   )
