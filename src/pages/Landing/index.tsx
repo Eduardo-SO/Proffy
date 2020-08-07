8dc05a4b87123a7f59a2685acc02f96c99e7f2ec
Eduardo SO <eduardo.sool07@gmail.com>
2020-08-07 19:35:49 -0300

    Initial Landing page structure

---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container, Title } from './styles';
+
+const Landing: React.FC = () => {
+  return (
+    <Container>
+      <Title>Hello Landing!</Title>
+    </Container>
+  )
+};
+
+export default Landing;
---
+++ src/pages/Landing/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: #63f;
+  align-items: center;
+  justify-content: center;
+  padding: 40px;
+`;
+
+export const Title = styled.Text`
+  color: #fff;
+  font-size: 80px;
+`;
