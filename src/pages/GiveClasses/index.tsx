8cc79697e3eb9a59156a79979e634a9c40aad5d1
Eduardo SO <eduardo.sool07@gmail.com>
2020-08-07 23:23:13 -0300

    Add GiveClasses page

---
+++ src/pages/GiveClasses/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Image } from 'react-native';
+
+import {
+   Container,
+   Title
+  } from './styles';
+
+// import landingImage from '../../assets/images/landing.png';
+
+const GiveClasses: React.FC = () => {
+  return (
+    <Container>
+      <Title>Seja bem-vindo!</Title>
+    </Container>
+  )
+};
+
+export default GiveClasses;
