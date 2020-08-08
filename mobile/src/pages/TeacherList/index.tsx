b609609804b80de6cdcab254e10c0595fd6d8a7e
Eduardo SO <eduardo.sool07@gmail.com>
2020-08-08 02:12:49 -0300

    Create TeacherList page

---
+++ mobile/src/pages/TeacherList/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { 
+  Container, 
+  Title,
+} from './styles';
+
+const TeacherList: React.FC = () => {
+  return (
+    <Container>
+      <Title>
+        Teacher list
+      </Title>
+    </Container>
+  );
+}
+
+export default TeacherList;
