45a0330132e69f304f11a5e1948b9e6748060295
Eduardo SO <eduardo.sool07@gmail.com>
2020-08-08 02:03:20 -0300

    Finish GiveClasses page

--- mobile/src/pages/GiveClasses/index.tsx
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -1,17 +1,38 @@
-import React from 'react';
-import { Image } from 'react-native';
+import React, { useCallback } from 'react';
+import { useNavigation } from '@react-navigation/native';
 
 import {
    Container,
-   Title
+   Content,
+   Title,
+   Description,
+   OkButton,
+   OkButtonText,
   } from './styles';
 
-// import landingImage from '../../assets/images/landing.png';
+import giveClassesBgImage from '../../assets/images/give-classes-background.png';
 
 const GiveClasses: React.FC = () => {
+  const { goBack } = useNavigation();
+
+  const handleGoBack = useCallback(() => {
+    goBack();
+  }, []);
+
   return (
     <Container>
-      <Title>Seja bem-vindo!</Title>
+      <Content resizeMode="contain" source={giveClassesBgImage}>
+        <Title>Quer ser um Proffy?</Title>
+        <Description>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+          Ipsam magni corporis delectus quis consectetur officiis animi, maiores tempora laudantium ex!
+        </Description>
+
+      </Content>
+
+        <OkButton onPress={handleGoBack}>
+          <OkButtonText>Tudo bem</OkButtonText>
+        </OkButton>
     </Container>
   )
 };
--- mobile/src/pages/GiveClasses/styles.ts
+++ mobile/src/pages/GiveClasses/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
   flex: 1;
@@ -9,12 +10,46 @@ export const Container = styled.View`
   background-color: #8257e5;
 `;
 
+export const Content = styled.ImageBackground`
+  flex: 1;
+  justify-content: center;
+`;
+
 export const Title = styled.Text`
-  margin-top: 80px;
+  max-width: 180px;
 
   color: #fff;
 
+  font-family: 'Archivo_700Bold';
+  font-size: 32px;
+  line-height: 37px;
+`;
+
+export const Description = styled.Text`
+  max-width: 240px;
+  margin-top: 24px;
+
+  color: #d4c2ff;
+
   font-family: 'Poppins_400Regular';
-  font-size: 20px;
-  line-height: 30px;
+  font-size: 16px;
+  line-height: 26px;
+`;
+
+export const OkButton = styled(RectButton)`
+  align-items: center;
+  justify-content: center;
+
+  height: 58px;
+  margin: 40px 0;
+  border-radius: 8px;
+
+  background: #04d361;
+`;
+
+export const OkButtonText = styled.Text`
+  color: #fff;
+
+  font-family: 'Archivo_700Bold';
+  font-size: 16px;
 `;
