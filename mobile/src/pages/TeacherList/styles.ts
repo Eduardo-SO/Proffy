b2fe7a157ced2e17c840ed640440bdd8ace3c1bc
Eduardo SO <eduardo.sool07@gmail.com>
2020-08-08 21:19:00 -0300

    Reset Container styles

--- mobile/src/pages/TeacherList/styles.ts
+++ mobile/src/pages/TeacherList/styles.ts
@@ -2,11 +2,6 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
-
-  padding: 40px;
-
-  background-color: #8257e5;
 `;
 
 export const Title = styled.Text`
