2456a614e1a84a0d50fddcb1ef849327ae3c111d
Eduardo SO <eduardo.sool07@gmail.com>
2020-08-14 23:17:06 -0300

    Add connection model

--- server/src/controllers/ConnectionsController.ts
+++ server/src/controllers/ConnectionsController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
 
+import Connection from '../models/Connection';
+
 import db from '../database/connection';
 
 export default class ConnectionsController {
@@ -14,9 +16,9 @@ export default class ConnectionsController {
   async create(request: Request, response: Response) {
     const { user_id } = request.body;
 
-    await db('connections').insert({
-      user_id
-    });
+    const connection = new Connection(user_id);
+
+    await db('connections').insert(connection);
 
     return response.status(201).send();
   }
---
+++ server/src/models/Connection.ts
@@ -0,0 +1,11 @@
+class Connection {
+  user_id: string;
+
+  constructor(
+    user_id: string,
+  ) {
+    this.user_id = user_id;
+  }
+}
+
+export default Connection;
