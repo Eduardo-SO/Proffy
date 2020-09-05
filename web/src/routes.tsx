b526789e10fc12e38166598fe26b0fbcaeca35a4
Eduardo SO <eduardo.sool07@gmail.com>
2020-09-04 23:33:43 -0300

    Change SignIn to default page

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -9,8 +9,8 @@ import TeacherForm from './pages/TeacherForm';
 const Routes: React.FC = () => {
   return (
     <BrowserRouter>
-      <Route path="/" exact component={Landing} />
-      <Route path="/signin" component={SignIn} />
+      <Route path="/" exact component={SignIn} />
+      <Route path="/landing" component={Landing} />
       <Route path="/study" component={TeacherList} />
       <Route path="/give-classes" component={TeacherForm} />
     </BrowserRouter>
